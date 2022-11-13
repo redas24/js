5567aafe2793d1ae34c275d86d0e8eaea349bcd4
redas24 <lchlebosz@gmail.com>
2022-11-13 15:55:32 +0000

    master

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-
-//Numbers
-
-//An integer is a whole number
-const myNumber = 42;
-const myFloat = 42.0151;
-const myString = "42.123abc"
-
-//chaining = using several methods chained together
+let canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 250;
 
 
-console.log(Number.parseFloat("4.237abc").toFixed(2).toString());
-
-//NaN is acronym for Not a Number
-
-//Math Methods and Properties 
-
-console.log(Math.PI)
-console.log(Math.trunc(Math.PI));
-console.log(Math.round(3.5));
-console.log(Math.floor(Math.PI));
-console.log(Math.pow(5, 2));
-console.log(Math.random());
-
-// Your First Code exercise 
-
-const anyName = "Lucanos"
-
-console.log("Luca".charAt(Math.floor(Math.random() *4)));
-console.log("Luca".charAt(Math.floor(Math.random() *4)));
-console.log("Luca".charAt(Math.floor(Math.random() *4)));
-console.log("Luca".charAt(Math.floor(Math.random() *4)));
-console.log("Luca".charAt(Math.floor(Math.random() *4)));
-
+let ctx = canvas.getContext('2d');
+ctx.font = '30px Cursive';
+ctx.fillText ("JavaScript learning path", 50, 50);
+document.body.appendChild(canvas);
