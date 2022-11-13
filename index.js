eeac3993e2013c957c7007eb01d93e4e03106b66
redas24 <lchlebosz@gmail.com>
2022-11-13 20:10:06 +0000

    master

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let canvas = document.createElement('canvas');
 canvas.width = 500;
 canvas.height = 250;
@@ -9,3 +11,21 @@ ctx.fillText ("JavaScript learning path", 50, 50);
 document.body.appendChild(canvas);
 
 //JSON: JavaScript Object Notation
+
+const myObj = { 
+    name: "Lucas",
+    hobbies: ["coding","hacking","music"],
+    hello: function() {
+        console.log("Hello!");
+    }
+}
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
