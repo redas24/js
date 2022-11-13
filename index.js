570a59866399711bcf0983c06536aed50dc5a28b
redas24 <lchlebosz@gmail.com>
2022-11-13 17:15:27 +0000

    master

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ let ctx = canvas.getContext('2d');
 ctx.font = '70px Time New Romance';
 ctx.fillText ("JavaScript learning path", 50, 50);
 document.body.appendChild(canvas);
+
+//JSON: JavaScript Object Notation
