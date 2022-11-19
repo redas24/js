a0d0691d499ad792312ac124883c6febbef53e23
redas24 <lchlebosz@gmail.com>
2022-11-19 20:28:02 +0000

    master

--- js/index.js
+++ js/index.js
@@ -44,3 +44,9 @@ const person = {
 
 console.log(person);
 
+
+document.getElementById("button").onclick = function() {
+    document.getElementById("confirm").innerHTML = "Order placed. Check email for confirmation ";
+    document.getElementById("button").style.display = "none";
+}
+
