6ec97da1be6261edd0d26543bd16e87e0550f773
redas24 <lchlebosz@gmail.com>
2022-11-23 12:13:02 +0000

    master

--- js/index.js
+++ js/index.js
@@ -50,3 +50,58 @@ document.getElementById("button").onclick = function() {
     document.getElementById("button").style.display = "none";
 }
 
+
+
+let foo = 42; // can reassign can build block scope
+foo = 78;
+console.log(foo);
+
+//let username = prompt();
+//console.log(username);//
+
+const baz = 42; // cant reassign
+
+//Data types , boolean, numbers,integers,string, undefined,null,symbol
+
+let string = "hey";
+
+console.log(string);
+
+
+let array = [5,'car', true];
+let array2 = new Array();
+array[0];
+console.log(array);
+array.push(array);
+// maths = assignment operators
+
+let postfix = 5;
+let prefix = 5;
+console.log(postfix);
+console.log(++prefix);
+console.log(prefix);
+console.log(postfix++);
+console.log(++postfix);
+// Assignment operators
+
+let additionAssignment = 1;
+additionAssignment += 2;
+console.log(additionAssignment);
+
+let subtractionAssignment =1;
+subtractionAssignment -= 2;
+// Arithmetic operators
+// Assignment operators(= += -= *=/=)
+// Comparison operators(== === < <= > => != !==)
+// Be wary of type-coercion
+
+// Advanced operators part2
+// Unary operators - operators with only single operand
+let test = {
+  x: 'x'
+
+};
+
+console.log(test.x);
+console.log(true&&true);
+console.log(true || true);
