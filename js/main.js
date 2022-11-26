fcdb9ae6b3b124f2acac959af18780e49806f8a3
redas24 <lchlebosz@gmail.com>
2022-11-26 16:21:36 +0000

    master

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* Function
+  - A function is a named group of expressions
+  - A function can be invoked at any time to execute the expressions it contains.
+  */
+
+(function () {
+
+  function function1() {
+    console.log('function1 invoked');
+  }
+  function1();
+})();
