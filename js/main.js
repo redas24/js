74e84b89868e3a3f778adebec629c5a474a40afd
redas24 <lchlebosz@gmail.com>
2022-11-27 12:51:51 +0000

    master

--- js/main.js
+++ js/main.js
@@ -5,8 +5,36 @@ Arrays cn be created in multiple ways :
       -We can create array literals
 */
 
-(function(){
+
+  let myArray = new Array();
+  let myArray2 = ['a value','a second value'];
+console.log(myArray2);
+console.log(myArray);
 
 
-  let myArray = new Array(1);
+// Objects
+(function() {
+
+  let myObject = {
+    car1: 'sport',
+    car2: false
+  };
+
+  let obj = new Object();
+
+  myObject.car2;
+  myObject['car2'];
+
+  myObject.car3 = true;
+  myObject['car4'] = 'engine';
+
+
+  function getThingByColor(color) {
+    let things = {
+    red:'red things',
+    green: 'green things',
+    yellow: 'yellow things'
+  };
+  return things[color] || 'Sorry no thing of that color exists!';
+  }
 }());
