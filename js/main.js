28718921c1b32df3807069689a31bc4def00df10
redas24 <lchlebosz@gmail.com>
2022-11-27 21:29:45 +0000

    master

--- js/main.js
+++ js/main.js
@@ -12,4 +12,17 @@ console.log(testRing.length);
 let ringArr = testRing.split(' ');
 console.log(ringArr);
 console.log(testRing.indexOf('is'));
+console.log(testRing.lastIndexOf('is'));
+console.log(testRing.toUpperCase());
+console.log(testRing.substring(8, 9));
+console.log(testRing.slice(-2))
+console.log('  test   '.trim());
 
+/* Summary Strings:
+- Use length property to get length of string
+- Use split to turn into an array
+- Use indexOf or lastIndexOf to find position od characters in a string
+- Use toUpperCase or to LowerCase methods for all caps/no caps
+- Use subStr or slice to select parts of a string
+- Use trim to remove white-space characters
+*/
