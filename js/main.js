3168f13e4678e81fd25367b9dee2065ed2b84e58
redas24 <lchlebosz@gmail.com>
2022-11-27 09:49:40 +0000

    master

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Arrays
+/* An array is single object which can contain multiple values
+Arrays cn be created in multiple ways :
+      -We can use the Array constructor
+      -We can create array literals
+*/
+
+(function(){
+
+
+  let myArray = new Array(1);
+}());
