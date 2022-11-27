44e6688c5676781c00c13412e94206f7a900d2e7
redas24 <lchlebosz@gmail.com>
2022-11-27 18:49:45 +0000

    master

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Working with strings
+/*Strings can be created in different ways:
+- 'As literal strings'- preferred
+- As object returned from the constructor
+- JavaScript performs type coercion under the hood when calling methods on primitives
+*/
+
+
+let testRing = 'This is a test ring';
+console.log(testRing.length);
