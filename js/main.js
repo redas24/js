fc1458e3b8c77b6c28176991caef37c242c4970b
redas24 <lchlebosz@gmail.com>
2022-11-27 18:16:32 +0000

    master

--- js/main.js
+++ js/main.js
@@ -114,3 +114,40 @@ for (let prop in obj) {
   if (obj.hasOwnProperty(prop))
   console.log('key is:',prop, 'value is:', obj[prop]);
 }
+// Iterators & Iterables
+/* The for of loop can be used with any iterable
+- An array is an iterable
+- a string is an iterable
+- arguments
+- The default behavior can be overridden
+- Some non-iterables can be made iterable, e.g object literals
+- An iterator is an object that provides access to a collection of items one at a time, and keeps track of which items have been accessed.
+- All iterator contain a next method this method return an object with 2 keys:
+                value - the current value of the iterable
+                done - true when there are no more values
+*/
+
+
+let mySet = new Set(['a', 'b', 'c']);
+let setEntries = mySet.entries();
+console.log(setEntries.next());
+console.log(setEntries.next());
+console.log(setEntries.next());
+console.log(setEntries.next());
+/*Summary: Iterators
+- Any object that exposes a next method
+- Next method returns object containing current value of collection being iterated
+- Just like loops, but we control when the next value is returned
+ */
+
+// while Loops
+/*while loops continue to execute while a condition remains true
+- constructor: while(condition) {} */
+
+
+
+let count = 0;
+
+while (count < 5) {
+  console.log(count += 1);
+}
