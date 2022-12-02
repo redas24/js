c5a128e715c0e5bb365d5eda52fd16ff4ee0add8
redas24 <lchlebosz@gmail.com>
2022-12-02 23:04:31 +0000

    master

--- js/main.js
+++ js/main.js
@@ -5,18 +5,17 @@
 - JavaScript performs type coercion under the hood when calling methods on primitives
 */
 
-
-let testRing = 'This is a test ring';
+let testRing = "This is a test ring";
 console.log(testRing.length);
 
-let ringArr = testRing.split(' ');
+let ringArr = testRing.split(" ");
 console.log(ringArr);
-console.log(testRing.indexOf('is'));
-console.log(testRing.lastIndexOf('is'));
+console.log(testRing.indexOf("is"));
+console.log(testRing.lastIndexOf("is"));
 console.log(testRing.toUpperCase());
 console.log(testRing.substring(8, 9));
-console.log(testRing.slice(-2))
-console.log('  test   '.trim());
+console.log(testRing.slice(-2));
+console.log("  test   ".trim());
 
 /* Summary Strings:
 - Use length property to get length of string
@@ -26,40 +25,53 @@ console.log('  test   '.trim());
 - Use subStr or slice to select parts of a string
 - Use trim to remove white-space characters
 */
-const number = [2,6,3,7,8,4,0,9];
+const number = [2, 6, 3, 7, 8, 4, 0, 9];
 number.sort();
 console.log(number);
 const array1 = [1, 2, 3];
 console.log(array1.includes(2));
 
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+];
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-
-const result = words.filter(word => word.length > 6);
+const result = words.filter((word) => word.length > 6);
 console.log(result);
 
-
 const array2 = [5, 12, 8, 130, 44];
-const found = array2.find(element => element > 10);
+const found = array2.find((element) => element > 10);
 console.log(found);
 
 // Working with strings part2
-console.log('------test------'.trimStart());
-console.log('------test------'.trimEnd());
-console.log('test'.padStart(7));
-console.log('test'.padStart(7, '$'));
+console.log("------test------".trimStart());
+console.log("------test------".trimEnd());
+console.log("test".padStart(7));
+console.log("test".padStart(7, "$"));
 // Template Literals
-let template = 'this is template literal'
-let a = '5' , b ='something';
-console.log('a is ${a} while b is ${b} else');
+let template = "this is template literal";
+let a = "5",
+  b = "something";
+console.log("a is ${a} while b is ${b} else");
 
 function reverse(string) {
-  return string.split('').reverse().join('');
+  return string.split("").reverse().join("");
 }
 
 function buildStr(strings, ...value) {
-  return strings[3] + value[2] +strings[2] +value[1] + strings[1] + value[0] +strings[0];
-
+  return (
+    strings[3] +
+    value[2] +
+    strings[2] +
+    value[1] +
+    strings[1] +
+    value[0] +
+    strings[0]
+  );
 }
 console.log(buildStr);
 
@@ -79,9 +91,9 @@ Template Literals
 - MAX_SAFE_INTEGER
 - MIN_SAFE_INTEGER
  */
- console.log(Number.isNaN(5 +'oops'));
-let floatString = '1.5';
-let intString = '3';
+console.log(Number.isNaN(5 + "oops"));
+let floatString = "1.5";
+let intString = "3";
 console.log(typeof Number.parseFloat(floatString));
 console.log(typeof Number.parseInt(intString));
 let num = 4.6789432;
@@ -89,59 +101,54 @@ console.log(num.toFixed(2));
 console.log(num.toExponential(2));
 console.log(num.toLocaleString(2));
 console.log(num.toPrecision(2));
-console.log(num.toString(2))
+console.log(num.toString(2));
 // Working with BIGInt
 // JAvaScript has a maximum safe value for integers 2*53-1
 
-
-
-  Number.MAX_SAFE_INTEGER + 1 === Number.MIN_SAFE_INTEGER + 2
-
-
-
-
-
-
+Number.MAX_SAFE_INTEGER + 1 === Number.MIN_SAFE_INTEGER + 2;
 
 // Working with Arrays
 
-let arr = ['one', 'two', 'three'];
+let arr = ["one", "two", "three"];
 arr.length;
 console.log(arr.slice());
-arr.push('four');
+arr.push("four");
 console.log(arr);
 
-
-arr.unshift('zero');
+arr.unshift("zero");
 console.log(arr);
 arr.pop();
 arr.shift();
 arr.splice(0, 1);
 console.log(arr);
-arr.reverse('a','b','c');
+arr.reverse("a", "b", "c");
 arr.sort();
 console.log(arr);
-console.log(['a','b','C'].sort(function(a, b) {
-  let caselessA = a.toLowerCase(),
-  caselessB = b.toLowerCase();
-  if (caselessA < caselessB) {
-    return -1;
-  } else if (caselessA === caselessB) {
-    return 0;
-  } else {
-    return 1;
-  }
-}));
-console.log([3, 10000, 20].sort(function(a, b) {
-  return a - b;
-}));
+console.log(
+  ["a", "b", "C"].sort(function (a, b) {
+    let caselessA = a.toLowerCase(),
+      caselessB = b.toLowerCase();
+    if (caselessA < caselessB) {
+      return -1;
+    } else if (caselessA === caselessB) {
+      return 0;
+    } else {
+      return 1;
+    }
+  })
+);
+console.log(
+  [3, 10000, 20].sort(function (a, b) {
+    return a - b;
+  })
+);
 
-arr.join('x');
+arr.join("x");
 console.log(arr);
-arr.indexOf('b')
-arr.lastIndexOf('a');
+arr.indexOf("b");
+arr.lastIndexOf("a");
 arr.slice(1);
-arr.slice('-2')
+arr.slice("-2");
 // Summary
 /*Arrays
 - length shows how many elements in the array
@@ -155,30 +162,30 @@ arr.slice('-2')
 
 // Working with arrays part 2
 // Arrays contain numerous methods to iterate their elements in different ways.
-let array = ['x', 'y','z'];
-array.forEach(function(currentItem) {
+let array = ["x", "y", "z"];
+array.forEach(function (currentItem) {
   console.log(currentItem);
 });
-array.some(function(currentItem){
-  console.log('Iterating...');
-  return currentItem === 'y';
+array.some(function (currentItem) {
+  console.log("Iterating...");
+  return currentItem === "y";
 });
-array.every(function(currentItem){
-  console.log('Also Iterating...');
-  return currentItem !== 'x';
+array.every(function (currentItem) {
+  console.log("Also Iterating...");
+  return currentItem !== "x";
 });
 let arr1 = [1, 2, 3, 4, 5];
-let sum = arr1.reduce(function(previous, current){
+let sum = arr1.reduce(function (previous, current) {
   return previous + current;
 }, 0);
 console.log(sum);
 let refs = {
-  a: 'alpha',
-  b: 'beta',
-  g: 'gamma'
+  a: "alpha",
+  b: "beta",
+  g: "gamma",
 };
-let arr2 = ['a','b','g'];
-let result2 = arr2.map(function(currentItem){
+let arr2 = ["a", "b", "g"];
+let result2 = arr2.map(function (currentItem) {
   return refs[currentItem];
 });
 console.log(result2);
@@ -193,4 +200,42 @@ Iterating arrays
  */
 // Working with arrays part 3
 // includes() - does the array contain the value?
-const fruits = ['Apples','Oranges','Bananas'];
+const fruits = ["Apples", "Oranges", "Bananas"];
+console.log(fruits.includes("Apples"));
+console.log(fruits.includes("Apples", 1));
+// flat() -flatten a multidimensional array to a single dimension
+
+const multiArray = ["Apples", "Oranges", ["Raspberries", "Blueberries"]];
+console.log(multiArray.flat());
+
+const multiArray2 = ["Apples", "Oranges", ["Lemons", "Limes"]];
+console.log(multiArray2.flat(2));
+// Summary
+/*includes - does the array include the specified value
+flat - unwrap multidimensional arrays on n depth
+ */
+
+// Working with object type
+// The prototype property
+// All objects are descendants of Object
+// Other objects inherit properties from Object via the prototype
+// The prototype is a "live" object - changes to it are seen everywhere
+
+//let newObj = {};
+//Object.prototype.mySpecialProperty = "special!";
+//console.log(newObj.mySpecialProperty);
+
+//let myArr = ["first", "second", "third"];
+//if (!Array.prototype.head) {
+//  Array.prototype.head = function () {
+//    return this[0];
+//  };
+//  console.log(myArr.head());
+//}
+
+let newObj = {
+  prop1: "prop1",
+};
+Object.keys(newObj).forEach(function (item) {
+  console.log(newObj[item]);
+});
