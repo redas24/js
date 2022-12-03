37ba9c322f557f9d652c73bdae64833b5ae905f1
redas24 <lchlebosz@gmail.com>
2022-12-03 16:36:33 +0000

    master

--- js/main.js
+++ js/main.js
@@ -8,5 +8,17 @@ let obj = {
   method: function () {
     return "method";
   },
+  [x + y]: 'computed'
 };
 console.log(obj);
+
+let composedObj = Object.assign({}, obj, {someOtherProp: 'testing'});
+console.log(composedObj);
+// Summary
+/*Syntactic improvements to object literals
+- Shorthand property assignment
+- Computed property keys
+- Use Object.assign to merge objects
+ */
+
+// Working with the OBjects Instances
