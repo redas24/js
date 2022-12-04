ea729ee7f9c6d72e261e92265e183cc3a13700e4
redas24 <lchlebosz@gmail.com>
2022-12-04 13:28:21 +0000

    master

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 //  Getters and Setters
 // Special methods that are invoked automatically when a property is accessed or assigned to.
+let myObj = {
+  otherProp: "other",
+
+  get prop() {
+    console.log("accessing")
+  },
+}
