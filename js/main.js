507fc35ba5d683b53f1093ddbc226de3fd11e463
redas24 <lchlebosz@gmail.com>
2022-12-05 21:23:29 +0000

    master

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 //  Getters and Setters
 // Special methods that are invoked automatically when a property is accessed or assigned to.
-let myObj(4) = {
+let myObj = {
   otherProp: "other",
 
   get prop() {
     console.log("accessing prop")
     return this.otherProp
   },
+  set prop(val) {
+    console.log('setting other prop to ${val}')
+    this.otherProp = val
+
+  }
 }
-console.log(MyObj)
+console.log(myObj.prop)
+myObj.prop = 'new'
+console.log(myObj)
+// Summary
+/*Getters and Setters
+- Special properties that invoke methods when accessed or assigned
+- this points to the object they are within */
+
+// Custom types Object
+
+class Person {
+  constructor (name) {
+    this.name = name
+  }
+  greet() {
+    return `Hi, my name is ${this.name}`
+  }
+}
+
+
+let alex = new Person('alex')
+console.log('alex')
