8884e6fd8330145a8641f231e5d04e378a6b31d5
redas24 <lchlebosz@gmail.com>
2022-12-06 22:26:55 +0000

    master

--- js/main.js
+++ js/main.js
@@ -1,19 +1,8 @@
-// The math objects - The Math API
-/* - The Math object doesn't have a constructor
-   - It is a global static object containing useful properties and methods
- */
+// Working with dates and times
+/*Date objects must be created using new Date()
+ - A date object represents a specific instant in time
+ - Dates are the number of milliseconds to have elapsed since 1 january 1970
+*/
 
-
-   console.log(Math.PI)
-   console.log(Math.E)
-   console.log(Math.ceil(1,3))
-   console.log(Math.floor(3,9))
-   console.log(Math.round(4,4))
-   console.log(Math.max(1,2,3,4,5))
-   console.log(Math.min(1,2,3,4,5,6))
-   console.log(Math.abs(-8))
-   console.log(Math.sqrt(128))
-   console.log(Math.pow(3,2))
-   console.log(Math.floor(Math.random() *10))
-// Summary
-// Covered the properties and methods of the Math Object
+let now = new Date();
+console.log(now);
