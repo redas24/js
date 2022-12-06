7c966ad33c7a084fcedc617c1ea214fe50df46ab
redas24 <lchlebosz@gmail.com>
2022-12-06 22:17:13 +0000

    master

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// The math objects - The Math API
+/* - The Math object doesn't have a constructor
+   - It is a global static object containing useful properties and methods
+ */
+
+
+   console.log(Math.PI)
+   console.log(Math.E)
+   console.log(Math.ceil(1,3))
+   console.log(Math.floor(3,9))
+   console.log(Math.round(4,4))
+   console.log(Math.max(1,2,3,4,5))
+   console.log(Math.min(1,2,3,4,5,6))
+   console.log(Math.abs(-8))
+   console.log(Math.sqrt(128))
+   console.log(Math.pow(3,2))
+   console.log(Math.floor(Math.random() *10))
+// Summary
+// Covered the properties and methods of the Math Object
