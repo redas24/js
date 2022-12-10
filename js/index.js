c191ebc956f850a8e82c478f6fa93594117e462e
redas24 <lchlebosz@gmail.com>
2022-12-10 11:07:48 +0000

    master

--- js/index.js
+++ js/index.js
@@ -866,3 +866,76 @@ console.log(kristo)
    console.log(Math.floor(Math.random() *10))
 // Summary
 // Covered the properties and methods of the Math Object
+
+
+// Working with dates and times
+/*Date objects must be created using new Date()
+ - A date object represents a specific instant in time
+ - Dates are the number of milliseconds to have elapsed since 1 january 1970
+*/
+
+let now = new Date();
+console.log( now);
+let partyTime = new Date (946583788000);
+console.log(partyTime);
+let dob = new Date('june 2 1983')
+console.log(dob);
+
+let specialDate = new Date(1983,2, 6, 1990, 2, 19);
+console.log(specialDate);
+console.log(specialDate.getDate());
+console.log(specialDate.getMonth());
+console.log(specialDate.getFullYear());
+console.log(specialDate.getUTCDate());
+console.log(specialDate.getUTCMonth());
+console.log(specialDate.getUTCFullYear())
+console.log(specialDate.getDay())
+console.log(specialDate.getUTCDay())
+console.log(specialDate.toString())
+console.log(specialDate.toUTCString())
+console.log(specialDate.toDateString())
+console.log(specialDate.toTimeString())
+console.log(specialDate.toISOString())
+console.log(specialDate.toLocaleString())
+
+// Summary
+/*Use the Date object to work with dates and times:
+- Use the Date() constructor to create date objects
+- USe instance methods to get or set parts of the date and time
+ */
+
+
+// Working with the Array Type
+/* It used to be difficult to detect an array vs an object
+ the typeof operator returns object for both objects and arrays
+ We can use Array.isArray() instead */
+
+
+ let obj = {};
+ let arr = [];
+ console.log(Array.isArray(obj))
+ console.log(Array.isArray(arr))
+
+ let newArray = Array.from('string');
+ console.log(newArray)
+
+ function logArgs() {
+  let args = Array.from(arguments);
+  console.log(args);
+ }
+ logArgs('a', 'b', 'c');
+
+ let mapObj = {
+  1: 'one',
+  2: 'two',
+  3: 'three'
+ };
+ let numArr = [1, 2, 3];
+ let mappedArray = Array.from(numArr, function(item){
+  return this[item];
+ }, mapObj);
+ console.log(mappedArray);
+//  Summary
+/* Useful methods of the Array constructor:
+- isArray method - returns true if the object passed to it is an array
+- from method - create a new array from any iterable ,optional map */
