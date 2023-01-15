ae4d71ada5fded76acc814a18d6db138f5bf0037
redas24 <lchlebosz@gmail.com>
2023-01-15 20:22:23 +0000

    master refresh

--- portfolio/deploy/main.js
+++ portfolio/deploy/main.js
@@ -2,6 +2,7 @@
 // The global scope is the top-level scope and contains all other scopes
 // This window object in a browser
 // The global scope is visible to all JavaScript files loaded by a page
+
 (function () {
   let myVar = "test";
   function local() {
