ce0e4097a41952d940b068083d394661916fdcdd
redas24 <lchlebosz@gmail.com>
2023-01-21 14:13:28 +0000

    master

--- portfolio/deploy/main.js
+++ portfolio/deploy/main.js
@@ -2,19 +2,3 @@
 // The global scope is the top-level scope and contains all other scopes
 // This window object in a browser
 // The global scope is visible to all JavaScript files loaded by a page
-
-(function () {
-  let myVar = "test";
-  function local() {
-    var localVar = "local";
-    console.log(myVar, localVar);
-  }
-  local();
-})();
-let newArr = [1, 2, 34, 5];
-console.log(newArr);
-self.scope = newArr;
-
-setTimeout(function () {
-  alert("Welcome to my journey into Devops");
-}, 5000);
