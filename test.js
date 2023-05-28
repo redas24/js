837466867f12f6e89b8274043f25420725058891
redas24 <lchlebosz>
2023-05-28 21:25:44 +0100

    add test array to file

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log("hello from redas");
+let newArray = [];
+let newArray2 = [];
+let newArray3 = [];
+let newArray4 = [];
+
+function myFunction() {
+    if (newArray.length < 10) {
+        newArray.push("Lucas");
+        console.log(newArray);
+    } else if (newArray.length == 10) {
+        newArray2.push("Lucas");
+        console.log(newArray2);
+    }
+
+}
+Array.prototype.myFunction = myFunction;
+myFunction();
